d09a49c4f933121897d6a242a27569c6259bec49
nutz98 <iit18022@std.uwu.ac.lk>
2022-07-24 10:20:41 +0530

    Removed navbar logo

--- admin/index.php
+++ admin/index.php
@@ -42,7 +42,7 @@ or die("There are no records to count ... \n" . mysqli_error());
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+  <link rel="icon" type="image/png" href="">
   <title>
     Admin | WFS
   </title>
@@ -63,7 +63,7 @@ or die("There are no records to count ... \n" . mysqli_error());
     <div class="sidenav-header">
       <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
       <a class="navbar-brand m-0" href="#">
-        <img src="./assets/img/logo-ct.png" class="navbar-brand-img h-100" alt="main_logo">
+        <img src="" class="navbar-brand-img h-100" alt="">
         <span class="ms-1 font-weight-bold">WFS</span>
       </a>
     </div>
